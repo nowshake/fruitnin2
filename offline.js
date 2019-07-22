9e64fe4bfc7aed056104e6b9859b2ac168588bac
Abhilash Badgujar <abhilashbadgujar.official.020@gmail.com>
2019-07-22 11:15:52 +0530

    push1

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1563722931,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/full-sheet0.png",
+		"images/ban-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/straw_half_one-sheet0.png",
+		"images/straw_half_2-sheet0.png",
+		"images/ban1-sheet0.png",
+		"images/ban2-sheet0.png",
+		"images/destroyer-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
